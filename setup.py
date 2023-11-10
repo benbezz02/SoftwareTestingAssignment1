d9aacfa430a611bf4eac6a7f9528e02aab7ec508
BenBezzina <benbezz02@gmail.com>
2023-11-10 09:58:30 +0100

    setup change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="Testing_Assignment_1",
     version="1.0.0",
-    packages=["src", "src.tests"],
+    packages=["src", "tests"],
     install_requires=["requests", "pytest", "mock", "datetime", "coverage"],
     python_requires=">=3.3",
 )
